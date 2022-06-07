06a0b447ceadfe80f61c7a63463f72b5433db91c
kimjinhyuk <kimjinhyuk2@gmail.com>
2022-06-08 00:56:39 +0900

    add agolia search api

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,8 +2,12 @@ export default {
   title: `Jin's Wiki`,
   description: "Jin's wiki for development",
   themeConfig: {
+
+
     siteTitle: `Jin's Wiki`,
+
     logo: '/images/j-letter.png',
+
     nav: [
       { text: "React", link: "/React/" },
       { text: "Vue", link: "/Vue/" },
@@ -11,9 +15,22 @@ export default {
       { text: "TS", link: "/TS/" },
       { text: "기타", link: "/etc/" },
     ],
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/kimjinhyuk' },
     ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present Jin'
+    },
+
+    algolia: {
+      appId: '2PYJFF934D',
+      apiKey: 'd93ad5208ca649e037e05221699fefd9',
+      indexName: 'jindocs'
+    },
+
     lastUpdated: "Last Updated",
   },
 }
