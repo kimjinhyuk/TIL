b89abd56c4db1ef26297a4140f915afaf6a8e008
kimjinhyuk <kimjinhyuk2@gmail.com>
2022-06-13 12:57:28 +0900

    add each introduce pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,20 +1,24 @@
 export default {
   title: `Jin's Wiki`,
   description: "Jin's wiki for development",
-  themeConfig: {
 
+  lastUpdated: true,
+
+  themeConfig: {
 
     siteTitle: `Jin's Wiki`,
 
     logo: '/images/j-letter.png',
 
-    nav: [
-      { text: "React", link: "/React/" },
-      { text: "Vue", link: "/Vue/" },
-      { text: "Svelte", link: "/Svelte/" },
-      { text: "TS", link: "/TS/" },
-      { text: "기타", link: "/etc/" },
-    ],
+    nav: nav(),
+
+    sidebar: {
+      '/react/': sidebarReact(),
+      '/vue/': sidebarVue(),
+      '/svelte/': sidebarSvelte(),
+      '/typescript/': sidebarTypeScript(),
+      '/etc/': sidebarETC()
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/kimjinhyuk' },
@@ -33,4 +37,84 @@ export default {
 
     lastUpdated: "Last Updated",
   },
+}
+
+function nav() {
+  return [
+    { text: "React/NextJS", link: "/react/what-is-vitepress", activeMatch: '/react/' },
+    { text: "Vue/NuxtJS", link: "/vue/introduction", activeMatch: '/vue/' },
+    { text: "Svelte/SvelteKit", link: "/svelte/introduction", activeMatch: '/svelte/' },
+    { text: "TS", link: "/typescript/introduction", activeMatch: '/typescript/'},
+    { text: "기타", link: "/etc/introduction", activeMatch: '/etc/' },
+  ]
+}
+
+function sidebarReact() {
+  return [
+    {
+      text: 'React',
+      collapsible: true,
+      items: [
+        { text: 'What is VitePress?', link: '/react/what-is-vitepress' },
+      ]
+    },
+    {
+      text: 'NextJS',
+      collapsible: true,
+      items: [
+        { text: 'NextJS', link: '/react/markdown' },
+      ]
+    },
+  ]
+}
+
+function sidebarVue() {
+  return [
+    {
+      text: 'Vue',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/vue/introduction' },
+        { text: 'App Configs', link: '/vue/app-configs' },
+        { text: 'Theme Configs', link: '/vue/theme-configs' },
+        { text: 'Frontmatter Configs', link: '/vue/frontmatter-configs' }
+      ]
+    }
+  ]
+}
+
+function sidebarSvelte() {
+  return [
+    {
+      text: 'Svelte',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/svelte/introduction' },
+      ]
+    }
+  ]
+}
+
+function sidebarTypeScript() {
+  return [
+    {
+      text: 'TypeScript',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/typescript/introduction' },
+      ]
+    }
+  ]
+}
+
+function sidebarETC() {
+  return [
+    {
+      text: '기타',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/etc/introduction' },
+      ]
+    }
+  ]
 }
